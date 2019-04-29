371fc6f52c9be89dda4f2ad88d05495280008c6b
曾庆雷 <zengql@zhongan.com.cn>
2019-04-29 15:36:26 +0800

    升级typescript版本

--- src/pages/page-routes.tsx
+++ src/pages/page-routes.tsx
@@ -6,27 +6,32 @@ import { DefaultLayout } from "../layout/default/default";
 import { AuthorizedRoute } from "../core/components/auth-route";
 import { UserRoutes } from "./user/user-routes";
 
+const DefaultRoutes = (props: { match: match }) => (
+  <DefaultLayout>
+    <Switch>
+      <Route exact path={props.match.path}>
+        <Redirect to={`${props.match.path}dashboard`} />
+      </Route>
+      <Route
+        exact
+        path={`${props.match.path}dashboard`}
+        component={DashboardIndex}
+      />
+      <Route component={NotFoundException} />
+    </Switch>
+  </DefaultLayout>
+);
+
 /** 页面路由集合 */
 export const PageRoutes = withRouter((props: any) => {
-  const defaultRoutes = (props: { match: match }) => (
-    <DefaultLayout>
-      <Switch>
-        <Route exact path={props.match.path}>
-          <Redirect to={`${props.match.path}dashboard`} />
-        </Route>
-        <Route
-          exact
-          path={`${props.match.path}dashboard`}
-          component={DashboardIndex}
-        />
-        <Route component={NotFoundException} />
-      </Switch>
-    </DefaultLayout>
-  );
   return (
     <Switch>
       <Route path={`${props.match.path}authorize/`} component={UserRoutes} />
-      <AuthorizedRoute path={`${props.match.path}`} component={defaultRoutes} {...props} />
+      <AuthorizedRoute
+        path={`${props.match.path}`}
+        component={DefaultRoutes}
+        {...props}
+      />
     </Switch>
   );
 });
